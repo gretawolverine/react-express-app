efa473d2cd822b29126c2d521a0a625aabb21981
greta.lucchina <greta.lucchina@mondora.com>
2022-08-12 11:26:29 +0200

    fix testDB route

--- api/routes/testDB.js
+++ api/routes/testDB.js
@@ -1,6 +1,6 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 
 // Variable to be sent to Frontend with Database status
 let databaseConnection = "Waiting for Database response...";
@@ -10,7 +10,10 @@ router.get("/", function (req, res, next) {
 });
 
 // Connecting to MongoDB
-mongoose.connect("mongodb://mongodb:27017/test", { useNewUrlParser: true });
+mongoose.connect("mongodb://mongodb:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // If there is a connection error send an error message
 mongoose.connection.on("error", (error) => {
